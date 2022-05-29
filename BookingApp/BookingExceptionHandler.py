0c26e247e0681a4821bf1e422c3f98b2d82dea3e
unknown <deveshsinghverma16@gmail.com>
2022-05-29 19:24:35 +0530

    Added Booking Endpoints

--- BookingApp/BookingExceptionHandler.py
+++ BookingApp/BookingExceptionHandler.py
@@ -4,3 +4,13 @@ class NOT_FOUND(APIException):
     status_code=404
     default_code="Not Found"
     default_detail="Not Found"
+
+class NOT_AUTHORIZED(APIException):
+    status_code= 403
+    default_code="NOT AUTHORIZED"
+    default_detail="NOT AUTHORIZED"
+
+class Bad_Request(APIException):
+    status_code=400
+    default_code = "Bad Request"
+    default_detail = "BAD REQUEST"
